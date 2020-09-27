6edb4277f37c0f46918ae426b02457774f49ad47
Vince Howard <vincenguyenhoward@gmail.com>
2020-09-27 16:26:23 -0600

    removed extra components and logs

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -34,7 +34,6 @@ const App = () => {
 	const portfolioMetaData = data.allPortfolioData.nodes || [];
 
 	const renderPortfolioData = () => {
-		console.log('portfolioMetaData:', portfolioMetaData);
 		return portfolioMetaData.map(
 			({ id, picture, title, description, projectInformation, repoData }: PortfolioCardProps) => (
 				<PortfolioCard key={id} {...{ id, picture, title, description, projectInformation, repoData }} />
